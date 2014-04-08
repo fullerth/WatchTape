1e5339f012290e137cd9a58f182ecc68d4e6866d
fullerth <fullerth@users.noreply.github.com>
2014-04-07 22:44:32 -0700

    Player and Bout viewing working. Need jam and video models next

--- WatchTape/player_list/models.py
+++ WatchTape/player_list/models.py
@@ -7,16 +7,16 @@ class Player(models.Model):
     def __str__(self):
         return("%s #%s" % (self.name, self.number))
 
-class PlayerToBout(models.Model):
-    player = models.ForeignKey(Player)
-    bout = models.ForeignKey('Bout')
-
-    def __str__(self):
-        return("%s in %s" % (self.player, self.bout))
-
 class Bout(models.Model):
     date = models.DateField('date played')
     location = models.CharField(max_length=200)
 
     def __str__(self):
         return("%s on %s" % (self.location, self.date))
+
+class PlayerToBout(models.Model):
+    player = models.ForeignKey(Player)
+    bout = models.ForeignKey('Bout')
+
+    def __str__(self):
+        return("%s in %s" % (self.player, self.bout))
--- WatchTape/player_list/views.py
+++ WatchTape/player_list/views.py
@@ -1,23 +1,39 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from player_list.models import Player, Bout, PlayerToBout
 
 #/player/<id>
-def view_bouts_by_player(context, player_id):
-    return HttpResponse("Viewing bouts of player id %s" % player_id)
+def view_bouts_by_player(request, player_id):
+    #get bout id's for all bouts played in by player
+    rostered_bouts = Bout.objects.filter(playertobout__player__id__iexact=player_id)
+    player = Player.objects.get(pk=player_id)
+    context = { 'player' : player, 'bouts' : rostered_bouts}
+    return render(request, 'player_list/bouts_by_player.html', context)
 
 #/bout/<id>
-def view_players_by_bout(context, bout_id):
-    return HttpResponse("Viewing players of bout id %s" % bout_id)
+def view_players_by_bout(request, bout_id):
+    rostered_players = Player.objects.filter(playertobout__bout__id__exact=bout_id)
+    bout = Bout.objects.get(pk=bout_id)
+    context = { 'players' : rostered_players, 'bout' : bout}
+    return render(request, 'player_list/players_by_bout.html', context)
 
-#/videos/bout/<id>
-def view_videos_by_bout(context, bout_id):
-    return HttpResponse("Viewing videos of bout id %s" % bout_id)
+#/video/bout/<id>
+def view_videos_by_bout(request, bout_id):
+    template = loader.get_template('player_list/videos_by_bout.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
 
-#/videos/player/<id>
-def view_videos_by_player(context, player_id):
-    return HttpResponse("Viewing videos of player id %s" % player_id)
+#/video/player/<id>
+def view_videos_by_player(request, player_id):
+    template = loader.get_template('player_list/videos_by_player.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
 
-#/videos/player_bout/<id>_<id>/
-def view_videos_by_player_and_bout(context, bout_id, player_id):
-    return HttpResponse("Viewing videos of player id %s at bout id %s" % (player_id, bout_id))
+#/video/player_bout/<id>_<id>/
+def view_videos_by_player_and_bout(request, bout_id, player_id):
+    template = loader.get_template('player_list/videos_by_player_and_bout.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
