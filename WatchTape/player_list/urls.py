dcd9c4d30d70c81747a424b64cb7c9bb9c4cbe35
Thomas <fullerth@gmail.com>
2014-05-05 19:14:50 -0700

    Large cleanup of templates and views. Reduced to bout/player/jam

--- WatchTape/player_list/urls.py
+++ WatchTape/player_list/urls.py
@@ -5,9 +5,9 @@ from player_list import views
 #already matched /watchtape/
 urlpatterns = patterns('',
     url(r'^player/(?P<player_id>\d+)/$',
-        views.view_bouts_by_player, name='bouts_by_player'),
+        views.view_player_info, name='player_info'),
     url(r'^bout/(?P<bout_id>\d+)/$',
-        views.view_players_by_bout, name='player_by_bout'),
-    url(r'^jam/(?P<jam_id>\d+)/$', views.view_players_by_jam,
-        name='players_by_jam'),
+        views.view_bout_info, name='bout_info'),
+    url(r'^jam/(?P<jam_id>\d+)/$', views.view_jam_info,
+        name='jam_info'),
     )
--- WatchTape/player_list/views.py
+++ WatchTape/player_list/views.py
@@ -6,48 +6,19 @@ from django.template import RequestContext, loader
 from player_list.models import Player, Bout, PlayerToBout, Jam, PlayerToJam
 
 #/player/<id>
-def view_bouts_by_player(request, player_id):
-    #get bout id's for all bouts played in by player
-#     rostered_bouts = \
-#             Bout.objects.filter(playertobout__player__id__iexact=player_id)
+def view_player_info(request, player_id):
     player = get_object_or_404(Player, pk=player_id)
-    context = { 'sort' : player, }#'items' : rostered_bouts,
-#                'sort_name' : player, 'item_name' : 'Bouts',
-#                'url_prefix' : 'bout'}
+    context = { 'player' : player, }
     return render(request, 'player_list/player.html', context)
 
 #/bout/<id>
-def view_players_by_bout(request, bout_id):
-    rostered_players = \
-            Player.objects.filter(playertobout__bout__id__exact=bout_id)
+def view_bout_info(request, bout_id):
     bout = get_object_or_404(Bout, pk=bout_id)
-    context = { 'sort' : bout, 'items' : rostered_players,
-                'sort_name' : bout, 'item_name' : 'Player',
-                'url_prefix': 'player'}
+    context = { 'bout' : bout, }
     return render(request, 'player_list/bout.html', context)
 
-#/video/bout/<id>
-def view_videos_by_bout(request, bout_id):
-    template = loader.get_template('player_list/videos_by_bout.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
-
-#/video/player/<id>
-def view_videos_by_player(request, player_id):
-    template = loader.get_template('player_list/videos_by_player.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
-
-#/video/player_bout/<id>_<id>/
-def view_videos_by_player_and_bout(request, bout_id, player_id):
-    template = loader.get_template('player_list/videos_by_player_and_bout.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
-
-def view_players_by_jam(request, jam_id):
-    players = Player.objects.filter(playertojam__jam__id__exact=jam_id)
+#/jam/<id>
+def view_jam_info(request, jam_id):
     jam = get_object_or_404(Jam, pk=jam_id)
-    context = {'sort' : jam, 'items' : players,
-               'sort_name' : jam, 'item_name' : 'Players',
-               'url_prefix' : 'player'}
+    context = {'jam' : jam, }
     return render(request, 'player_list/jam.html', context)
