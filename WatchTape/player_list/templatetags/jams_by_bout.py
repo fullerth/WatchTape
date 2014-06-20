7e10f9ea78283444d417a2e79282378244599dbf
Thomas <fullerth@gmail.com>
2014-06-20 01:00:14 -0700

    Fixed jams to show videos as subitems. Need to revamp the item_by_sort template

--- WatchTape/player_list/templatetags/jams_by_bout.py
+++ WatchTape/player_list/templatetags/jams_by_bout.py
@@ -9,15 +9,23 @@ register = template.Library()
 def jams_by_bout(bout_id):
     jams = Jam.objects.filter(bout__id__exact=bout_id)
     bout = get_object_or_404(Bout, pk=bout_id)
-    videos = VideoToJam.objects.filter(jam__in=jams)
-    temp_video = VideoToJam.objects.filter(jam__id__in=jams)
-    print(temp_video)
-    print(temp_video[0].timecode_url)
-    print(temp_video[1].timecode_url)
+    #videos = VideoToJam.objects.filter(jam__in=jams)
+    #temp_video = VideoToJam.objects.filter(jam__id__in=jams)
+
+    videos = []
+
+    print(jams)
+    for jam in jams:
+        print(jam.id)
+        videos.append(VideoToJam.objects.filter(jam__id__exact=jam.id))
+        #print(tmp_video)
+
+    item_subitem_zip = zip(jams, videos)
+    item_subitem_tuple = list(item_subitem_zip)
+    print(item_subitem_tuple)
 
     print('videos: {0}'.format(videos))
-    context = {'sort' : bout_id, 'items' : jams,
+    context = {'sort' : bout_id, 'items' : item_subitem_tuple,
                'sort_name' : bout, 'item_name' : 'Jams',
-               'url_prefix' : 'jam', 'subitems' : videos,
-               'include_video_by_jams' : temp_video}
+               'url_prefix' : 'jam', 'subitems' : True}
     return context
--- WatchTape/player_list/templatetags/jams_by_player.py
+++ WatchTape/player_list/templatetags/jams_by_player.py
@@ -1,7 +1,7 @@
 from django import template
 from django.shortcuts import render, get_object_or_404
 
-from player_list.models import Jam, Player
+from player_list.models import Jam, Player, VideoToJam
 
 register = template.Library()
 
@@ -9,7 +9,17 @@ register = template.Library()
 def jams_by_player(player_id):
     jams = Jam.objects.filter(playertojam__player__id__exact=player_id)
     player = get_object_or_404(Player, pk=player_id)
-    context = {'sort' : player_id, 'items' : jams,
+
+    videos = []
+
+    for jam in jams:
+            videos.append(VideoToJam.objects.filter(jam__id__exact=jam.id))
+
+    item_subitem_zip = zip(jams, videos)
+    item_subitem_tuple = list(item_subitem_zip)
+
+
+    context = {'sort' : player_id, 'items' : item_subitem_tuple,
                'sort_name' : player, 'item_name' : 'Jams',
-               'url_prefix' : 'jam'}
+               'url_prefix' : 'jam', 'subitems': True}
     return (context)
