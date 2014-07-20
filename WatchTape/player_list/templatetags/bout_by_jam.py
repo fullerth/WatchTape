ed9aa10191ac3f6aebf0ab46efec138362cffbae
Thomas <fullerth@gmail.com>
2014-07-20 00:41:39 -0700

    Adding custom top bar. Adding context to bout_by_jam template tags

--- WatchTape/player_list/templatetags/bout_by_jam.py
+++ WatchTape/player_list/templatetags/bout_by_jam.py
@@ -1,5 +1,6 @@
 from django import template
 from django.shortcuts import get_object_or_404
+from django.template import Context
 
 from player_list.models import Bout, Jam #JamToBout
 
@@ -9,7 +10,7 @@ register = template.Library()
 def bout_by_jam(jam_id):
     bout = Bout.objects.filter(jam__id__exact=jam_id)
     jam = get_object_or_404(Jam, pk=jam_id)
-    context = {'sort' : jam, 'items' : bout,
+    new_context = {'sort' : jam, 'items' : bout,
                'sort_name' : jam, 'item_name' : 'Bout',
                'url_prefix' : 'bout'}
-    return context
+    return Context(new_context)
