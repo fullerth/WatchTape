eb211b681eb18b46eeaf06051bef335ae8f7d502
fullerth <fullerth@gmail.com>
2014-07-26 00:08:37 -0700

    Adding working CSS and spacer. Fixed item by sort name to be dynamic

--- WatchTape/player_list/templatetags/jams_by_player.py
+++ WatchTape/player_list/templatetags/jams_by_player.py
@@ -13,19 +13,17 @@ def jams_by_player(player_id, half):
 
     videos = []
 
-    half = half+0
-    print('{0}'.format(half))
-
     for jam in jams:
             videos.append(VideoToJam.objects.filter(jam__id__exact=jam.id).
                                              filter(jam__half__exact=half))
 
     item_subitem_zip = zip(jams, videos)
     item_subitem_tuple = list(item_subitem_zip)
-
+    item_name = 'Half {0} Jams'.format(half)
 
 
     context = {'sort' : player_id, 'items' : item_subitem_tuple,
-               'sort_name' : player, 'item_name' : 'Jams',
-               'url_prefix' : 'jam', 'subitems': True}
+               'sort_name' : player, 'item_name' : item_name,
+               'url_prefix' : 'jam', 'subitems': True,
+               }
     return (context)
