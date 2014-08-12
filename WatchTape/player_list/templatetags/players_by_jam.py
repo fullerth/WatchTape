a09b63f5073ea9b5e7b5105e565f35013d2a9908
Thomas <fullerth@gmail.com>
2014-08-11 21:57:52 -0700

    Added a properties to the item_by_sort header to allow it to be hidden

--- WatchTape/player_list/templatetags/players_by_jam.py
+++ WatchTape/player_list/templatetags/players_by_jam.py
@@ -6,10 +6,10 @@ from player_list.models import Player, Jam
 register = template.Library()
 
 @register.inclusion_tag('player_list/item_by_sort.html')
-def players_by_jam(jam_id):
+def players_by_jam(jam_id, header_properties = ""):
     players = Player.objects.filter(jam__id__exact=jam_id)
     jam = get_object_or_404(Jam, pk=jam_id)
     context = {'sort' : jam, 'items' : players,
                'sort_name' : jam, 'item_name' : 'Players',
-               'url_prefix' : 'player'}
+               'url_prefix' : 'player', 'header_properties' : header_properties}
     return context
