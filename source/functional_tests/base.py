bbdf654a128b2d1f8f04d5f81bf8038568777ce2
Thomas <fullerth@gmail.com>
2015-01-04 02:06:03 -0800

    Moving functional tests into individual files

---
+++ source/functional_tests/base.py
@@ -0,0 +1,69 @@
+from django.test import LiveServerTestCase
+
+import sys, os
+from datetime import datetime
+
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
+from player_list.views import view_bout_info
+
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
+
+
+class FunctionalTest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
+    def setUp(self):
+        self.display = Display()
+        self.display.start()
+
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(10)
+
+    def tearDown(self):
+        if not self._outcomeForDoCleanups.success:
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
+        self.browser.quit()
+        self.display.stop()
+
+    def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('dumping page HTL to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+            folder = SCREEN_DUMP_LOCATION,
+            classname = self.__class__.__name__,
+            method = self._testMethodName,
+            windowid = self._windowid,
+            timestamp=timestamp
+        )
---
+++ source/functional_tests/test_jam_controls.py
@@ -0,0 +1,10 @@
+from .base import FunctionalTest
+
+class JamControlsTest(FunctionalTest):
+    def test_next_jam_control_functions(self):
+        #Foo sees the next jam control on the page
+
+        #Foo presses the next jam control
+
+        #The jam starts on the next jam time
+        self.fail('finish the test')
