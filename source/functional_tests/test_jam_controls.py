2d29e6ff6b7d259b3aa88b8f059a22f6c7b53a9f
Thomas <fullerth@gmail.com>
2015-01-14 11:14:43 -0800

    Adding extra formatting to template and functional test

--- source/functional_tests/test_jam_controls.py
+++ source/functional_tests/test_jam_controls.py
@@ -14,13 +14,16 @@ class JamControlsTest(FunctionalTest):
                '/watchtape/video_player/controller/']
         self.browser.get(''.join(url))
 
-        #Foo sees the info starts at jam 1
-
-        #Foo sees the next jam control on the page
+        #Foo sees the jam controls on the page
         next_jam = self.browser.find_element_by_id('id_next_jam')
+        prev_jam = self.browser.find_element_by_id('id_prev_jam')
 
-        #Foo presses the next jam control
 
-        #The data for the next jam is displayed
+        #Foo sees the info starts at jam 1
+        current_jam = self.browser.find_element_by_id('id_current_jam')
+        expected_jam = 1
+        self.assertEquals(current_jam.text, 'Jam {0}'.format(expected_jam))
+
+        #Foo presses next and prev controls and verifies current jam changes
 
         self.fail('finish the test')
