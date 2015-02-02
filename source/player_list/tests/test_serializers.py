cbe1a5696ddddb007629e682e48f9ef689834d8f
Thomas <fullerth@gmail.com>
2015-02-01 21:30:05 -0800

    Unit tests for VideoToJamSerializer Create and Update

--- source/player_list/tests/test_serializers.py
+++ source/player_list/tests/test_serializers.py
@@ -25,3 +25,29 @@ class VideoToJamSerializerTestCase(TestCase):
         self.assertIsInstance(v_to_j, VideoToJam,
                               msg="VideoToJamSerializer creating incorrect instance")
 
+    def test_update_a_video_to_jam_time(self):
+        dict = self._create_video_to_jam_data()
+        first_start_time = '0m10s'
+        first_end_time = '0m40s'
+        updated_start_time = '5m5s'
+        updated_end_time = '6m20s'
+        data = {'video' : dict['video'], 'jam' : dict['jam'],
+                'start_time' : first_start_time, 'end_time' : first_end_time}
+        v_to_j_serializer = VideoToJamSerializer()
+        v_to_j = v_to_j_serializer.create(data)
+
+        self.assertEqual(v_to_j.start_time, first_start_time,
+                         msg="Initial start time incorrect")
+        self.assertEqual(v_to_j.end_time, first_end_time,
+                         msg="Initial end time incorrect")
+
+        data['start_time'] = updated_start_time
+        data['end_time'] = updated_end_time
+
+        v_to_j_serializer.update(v_to_j, data)
+
+        self.assertEqual(v_to_j.start_time, updated_start_time,
+                         msg="Update to start time failed")
+        self.assertEqual(v_to_j.end_time, updated_end_time,
+                         msg="Update to end time failed")
+
