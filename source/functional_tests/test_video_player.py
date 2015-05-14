87771bcc8b3c6d04f11700590047ea58cfde8927
Thomas <fullerth@gmail.com>
2015-05-14 11:46:16 -0700

    Added test to fix iframe src error with no provided video url. Fixed jam time button test to find correct element

--- source/functional_tests/test_video_player.py
+++ source/functional_tests/test_video_player.py
@@ -10,26 +10,26 @@ class VideoPlayerTest(FunctionalTest):
         def __init__(self, server_url, browser):
             self._create_new_video_player()
             self._get_video_page(server_url, browser)
-            
+
         def _create_new_video_player(self):
             self.video_player = {}
             self.video_player['bout'] = Bout()
             self.video_player['bout'].save()
-    
+
             self.video_player['video'] = Video()
             self.video_player['video'].save()
-    
+
             self.video_player['jam'] = Jam(bout=self.video_player['bout'])
             self.video_player['jam'].save()
 
         def _get_video_page(self, server_url, browser):
-            url = [server_url, 
+            url = [server_url,
                    '/watchtape/video_player/video/{0}'.format(
                                                 self.video_player['video'].id),
                    ]
             browser.get(''.join(url))
-            
-            
+
+
 
     def test_video_player_title(self):
         '''Make sure that a video only, with no video_to_jams will display'''
@@ -40,25 +40,39 @@ class VideoPlayerTest(FunctionalTest):
 
     def test_jam_list_appears(self):
         '''Test to make sure that the jam list appears in the correct tab'''
-        self.VideoPlayerPageFactory(self.server_url, self.browser)        
+        self.VideoPlayerPageFactory(self.server_url, self.browser)
 
         #There is a tab with jams displayed below the video
         jam_tab = self.browser.find_element_by_id('id_jam_time_tab')
-        
+
         #Since there are no jams associated with this video the current data tab
         #does not appear
         with self.assertRaises(NoSuchElementException):
             self.browser.find_element_by_id('id_current_jam_tab')
-        
+
         #A button is now visible in the jam tab to add jam times
-        timing_button = jam_tab.find_element_by_id('id_jam_time_button')
-        
+        timing_button = self.browser.find_element_by_id('id_jam_time_button')
+
         #The user clicks the button
-        selenium.click(timing_button)
-        
+        timing_button.click()
+
         #The jam is added to the database with the current video time
-        
+
         #The jam appears in the jam tab under the jam list
         self.fail('finish the test!')
 
-    
+    def test_video_player_renders_with_no_video(self):
+        '''Test to make sure that lack of a video URL does not cause rendering problems'''
+        self.VideoPlayerPageFactory(self.server_url, self.browser)
+
+        #If there is a tag with the id "id_vimeo_player"
+        try:
+            vimeo_player = self.browser.find_element_by_id('id_vimeo_player')
+        except NoSuchElementException:
+            return
+
+        #Should never point to localhost, but it may if a malformed relative
+        #url is created for the src attribute
+        self.assertNotIn("localhost", vimeo_player.get_attribute("src"),
+            "video.player_url was not specified and the template did not handle \
+ it gracefully")
