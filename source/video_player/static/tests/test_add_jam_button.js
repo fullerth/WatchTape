14cba63e4b9008c8058a63d3497ab1505494b082
Thomas <fullerth@gmail.com>
2015-05-18 16:04:17 -0700

    Removing test to bind froogaloop to object, pushing that feature out till it's actually needed

--- source/video_player/static/tests/test_add_jam_button.js
+++ source/video_player/static/tests/test_add_jam_button.js
@@ -1,14 +1,4 @@
 test(
-'WatchTape.VideoPlayer.initialize binds froogaloop argument to it',
-function() {
-    var mockFroogaloop = {
-    }
-    WatchTape.VideoPlayer.initialize(mockFroogaloop)
-    
-    ok(WatchTape.VideoPlayer.froogaloop)
-});
-
-test(
 'initialize binds an #id_jam_time_button.onclick callback to \
 froogaloop.api(\'CurrentTime\') object ', function() {
     var currentTimeWasCalled = false;
