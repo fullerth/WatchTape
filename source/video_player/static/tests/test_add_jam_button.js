538055db7727a2ab8d9c2528fa2f3abd94974658
Thomas <fullerth@gmail.com>
2015-05-19 19:13:51 -0700

    Using a module for #id_jam_time_button tests

--- source/video_player/static/tests/test_add_jam_button.js
+++ source/video_player/static/tests/test_add_jam_button.js
@@ -1,32 +1,11 @@
-test(
-'initialize binds an #id_jam_time_button.onclick callback to \
-froogaloop.api(\'CurrentTime\') object ', function() {
-    var currentTimeWasCalled = false;
-    var mockCurrentTimeFunction = function() {currentTimeWasCalled = true; };
-    var mockFroogaloop = {
-        api: function(method) {
-            if(method = 'CurrentTime') { 
-                mockCurrentTimeFunction()
-            }
-        }
-    };
-    
-    WatchTape.VideoPlayer.initialize(mockFroogaloop);
-    
-    equal(currentTimeWasCalled, false, 
-          'Check current time function is not called before click');
-    $('#id_jam_time_button').trigger('click');
-    equal(currentTimeWasCalled, true, 
-          'Check current time function is called after click');
-});
-
-var token, requests, xhr;
+var token, requests, xhr, urls;
 module("id_jam_time_button onclick callback", {
     setup: function() {
         token = 'csrf token';
         mockFroogaloop = {
             api: sinon.mock()
         };
+        urls = { video_to_jam : 'video_to_jam'}
         xhr = sinon.useFakeXMLHttpRequest();
         requests = []
         xhr.onCreate = function(request) {requests.push(request)};
@@ -38,10 +17,21 @@ module("id_jam_time_button onclick callback", {
 })
 
 test(
+'initialize binds an #id_jam_time_button.onclick callback that calls the \
+froogaloop.api(\'CurrentTime\') object', function() {    
+    WatchTape.VideoPlayer.initialize(mockFroogaloop, urls);
+    
+    $('#id_jam_time_button').trigger('click');
+    
+    ok(mockFroogaloop.api.calledOnce, 'check add jam button logic');
+});
+
+
+test(
 'initialize #id_jam_time_button onclick callback performs a $.post operation \
 with the current time data from the video', function(){
-    WatchTape.VideoPlayer.initialize(mockFroogaloop);
-    console.log(requests)
+    WatchTape.VideoPlayer.initialize(mockFroogaloop, urls);
     $('#id_jam_time_button').trigger('click');
-    equal(requests.length, 1)
+    equal(requests.length, 1, 'check ajax request')
+    equal(requests[0].url, urls.video_to_jam, 'check url')
 });
