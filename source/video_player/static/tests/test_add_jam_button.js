5709e78e6304ed37cc001c641e1fc747f384e56b
Thomas <fullerth@gmail.com>
2015-05-20 12:30:48 -0700

    Reduced froogaloop mock to sinon.stub(), added tests for ajax request method and data

--- source/video_player/static/tests/test_add_jam_button.js
+++ source/video_player/static/tests/test_add_jam_button.js
@@ -3,7 +3,7 @@ module("id_jam_time_button onclick callback", {
     setup: function() {
         token = 'csrf token';
         mockFroogaloop = {
-            api: sinon.mock()
+            api: sinon.stub()
         };
         urls = { video_to_jam : 'video_to_jam'}
         xhr = sinon.useFakeXMLHttpRequest();
@@ -18,21 +18,24 @@ module("id_jam_time_button onclick callback", {
 
 test(
 'initialize binds an #id_jam_time_button.onclick callback that calls the \
-froogaloop.api(\'CurrentTime\') object', function() {    
+froogaloop.api(\'CurrentTime\') object', function() {
     WatchTape.VideoPlayer.initialize(mockFroogaloop, urls);
-    
+
     $('#id_jam_time_button').trigger('click');
-    
+
     ok(mockFroogaloop.api.calledOnce, 'check add jam button logic');
 });
-
 
 test(
 'initialize #id_jam_time_button onclick callback performs a $.post operation \
 with the current time data from the video', function(){
+
+
     WatchTape.VideoPlayer.initialize(mockFroogaloop, urls);
     $('#id_jam_time_button').trigger('click');
+
     equal(requests.length, 1, 'check ajax request')
     equal(requests[0].url, urls.video_to_jam, 'check url')
-    equal(requests[0].method, 'POST')
+    equal(requests[0].method, 'POST', 'check ajax request method')
+    equal(requests[0].requestBody, 'time=data', 'check ajax request data')
 });
--- source/video_player/static/video_player/jam_tagger.js
+++ source/video_player/static/video_player/jam_tagger.js
@@ -1,7 +1,7 @@
 var initialize = function(froogaloop, urls) {
     $('#id_jam_time_button').on('click', function() {
         froogaloop.api('CurrentTime')
-        $.post(urls.video_to_jam)
+        $.post(urls.video_to_jam, {'time' : 'data'})
     })
 }
 
