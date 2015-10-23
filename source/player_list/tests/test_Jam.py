09e2278ea63a09907564ab3041e110305c5ba364
Thomas <fullerth@gmail.com>
2015-10-23 14:23:08 -0700

    Add assert to jam __str__ test

--- source/player_list/tests/test_Jam.py
+++ source/player_list/tests/test_Jam.py
@@ -12,4 +12,6 @@ class JamModelTest(TestCase):
 	def test_jam_str_formats_with_blank_optional_fields(self):
 		bout = Bout.objects.create()
 		jam = Jam.objects.create(bout=bout)
-		jam.__str__()
+		jam_str = jam.__str__()
+		self.assertEqual(jam_str, "{0}, Half #{1}, Jam #{2}".format(
+			jam.bout, jam.half, jam.number)
