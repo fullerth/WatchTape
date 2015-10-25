10889f77b59465ea1d3bbacd3a6cad48a2f1edd7
Thomas <fullerth@gmail.com>
2015-10-25 15:31:27 -0700

    Add test for time values in video player context with one and two bouts

--- source/video_player/tests/test_views.py
+++ source/video_player/tests/test_views.py
@@ -5,56 +5,73 @@ from video_player.views import view_video_player
 from player_list.models import Video, Bout, Jam, VideoToJam, Roster, Team, Player, PlayerToRoster
 
 class VideoTaggingTests(TestCase):
-	def setUp(self):
-		self.video = Video.objects.create()
-		self.video.save()
-	
-	def _add_player(self):
-		player = Player.objects.create()
-		player.save()
-		return player
+    def setUp(self):
+        self.video = Video.objects.create()
+        self.video.save()
+    
+    def _add_player(self):
+        player = Player.objects.create()
+        player.save()
+        return player
 
-	def _add_bout(self, video):
-		home_team = Team.objects.create()
-		away_team = Team.objects.create()
-		home_roster = Roster.objects.create(team=home_team)
-		away_roster = Roster.objects.create(team=away_team)
+    def _add_bout(self, video, v_to_j_time='0h0m2s'):
+        home_team = Team.objects.create()
+        away_team = Team.objects.create()
+        home_roster = Roster.objects.create(team=home_team)
+        away_roster = Roster.objects.create(team=away_team)
 
-		bout = Bout.objects.create(home_roster=home_roster, 
-				away_roster=away_roster)
-		bout.save()
-		jam = Jam.objects.create(bout=bout)
-		jam.save()
-		video_to_jam = VideoToJam.objects.create(video=video,
-												 jam=jam)
-		video_to_jam.start_time='0h0m2s'
-		video_to_jam.save()
+        bout = Bout.objects.create(home_roster=home_roster, 
+                away_roster=away_roster)
+        bout.save()
+        jam = Jam.objects.create(bout=bout)
+        jam.save()
+        video_to_jam = VideoToJam.objects.create(video=video,
+                                                 jam=jam)
+        video_to_jam.start_time = v_to_j_time
+        video_to_jam.save()
 
-		return{'home_roster':home_roster, 'away_roster':away_roster, 'jam':jam}
-	
+        return{'home_roster':home_roster, 'away_roster':away_roster, 'jam':jam, 'video_to_jam':video_to_jam}
+    
 
-	def test_video_player_renders_with_malformed_database(self):
-		request = RequestFactory()
-		request.get(reverse('video_player', kwargs={'video_id':1}))
-		response = view_video_player(request, 1)
-		self.assertEqual(response.status_code, 200)
+    def test_video_player_renders_with_malformed_database(self):
+        request = RequestFactory()
+        request.get(reverse('video_player', kwargs={'video_id':1}))
+        response = view_video_player(request, 1)
+        self.assertEqual(response.status_code, 200)
 
-	def test_video_player_renders_with_empty_rosters(self):
-		self._add_bout(self.video)
-		response = self.client.get('/')
-		self.assertEqual(response.status_code, 200)
+    def test_video_player_renders_with_empty_rosters(self):
+        self._add_bout(self.video)
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
 
-	def test_video_player_context_contains_jams_in_one_bout(self):
-		bout_info = self._add_bout(self.video)
-		response = self.client.get('/')
-		jam_list = map(repr, [bout_info['jam']])
-		self.assertQuerysetEqual(response.context['jams'], jam_list) 
+    def test_video_player_context_contains_jams_in_one_bout(self):
+        bout_info = self._add_bout(self.video)
+        response = self.client.get('/')
+        jam_list = map(repr, [bout_info['jam']])
+        self.assertQuerysetEqual(response.context['jams'], jam_list) 
 
-	def test_video_player_context_contains_jams_in_two_bouts(self):
-		bout_1_info = self._add_bout(self.video)
-		bout_2_info = self._add_bout(self.video)
-		response = self.client.get('/')
-		jam_list = map(repr, [bout_1_info['jam'], bout_2_info['jam']])
-		self.assertQuerysetEqual(response.context['jams'], jam_list,
-			ordered=False)
-		
+    def test_video_player_context_contains_jams_in_two_bouts(self):
+        bout_1_info = self._add_bout(self.video)
+        bout_2_info = self._add_bout(self.video)
+        response = self.client.get('/')
+        jam_list = map(repr, [bout_1_info['jam'], bout_2_info['jam']])
+        self.assertQuerysetEqual(response.context['jams'], jam_list,
+            ordered=False)
+        
+    def test_video_player_context_contains_times_in_one_bout(self):
+        bout_info = self._add_bout(self.video)
+        response = self.client.get('/')
+        time_list = map(repr, [bout_info['video_to_jam'].start_seconds])
+        self.assertQuerysetEqual(response.context['times'], time_list)
+
+
+    def test_video_player_context_contains_times_in_two_bouts(self):
+        bout_1_info = self._add_bout(self.video, v_to_j_time='1h5m18s')
+        bout_2_info = self._add_bout(self.video, v_to_j_time='2h2m18s')
+        response = self.client.get('/')
+        time_list = map(repr, [bout_1_info['video_to_jam'].start_seconds,
+            bout_2_info['video_to_jam'].start_seconds])
+        self.assertQuerysetEqual(response.context['times'], time_list)
+
+
+
